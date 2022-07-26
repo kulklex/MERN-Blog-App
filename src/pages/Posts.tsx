c3b52af9366f3e8676e649eca2e257c59c768cb0
Kulklex <kulklex@gmail.com>
2022-07-26 23:12:03 +0100

    created BlogPosts component and styled it

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import "../App.css"
+import BlogPost from './BlogPost'
 
 
 export default function Posts() {
   return (
-    <div className='posts'>Posts</div>
+    <div className='posts flex flex-wrap m-5'>
+      <BlogPost/>
+      <BlogPost/>
+      <BlogPost/>
+      <BlogPost/>
+      <BlogPost/>
+      <BlogPost/>
+    </div>
   )
 }
