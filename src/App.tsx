11ef20b31897b61f75e56e76619cafd0918c52a8
Kulklex <kulklex@gmail.com>
2022-07-28 22:24:17 +0100

    react-router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import './App.css';
-import Navbar from './navbar/Navbar';
-import Home from './pages/Home';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./navbar/Navbar";
+import Home from "./pages/Home";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Settings from "./pages/Settings";
+import SinglePost from "./pages/SinglePost";
 
 function App() {
-  
-
-
-
+  const user = true
   return (
-    <div className="App">
-      <Navbar/>
-      <Home/>
-    </div>
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={user ? <Home /> : <Login/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/write" element={user ? <Write/> : <Login/>} />
+        <Route path="/settings" element={user ? <Settings/> : <Login/>} />
+        <Route path="/post/:id" element={<SinglePost/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
