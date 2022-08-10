08558c4175ddcab9b89294b7c1ecc64c2ca4da47
Kulklex <kulklex@gmail.com>
2022-08-10 22:59:22 +0100

    created category model

--- src/models/postModel.ts
+++ src/models/postModel.ts
@@ -4,9 +4,17 @@ export interface Post{
     categories: [string]
     desc: string
     email: string
-    name: string
+    name?: string
     photo?: string
     title: string
+    createdAt: string
+    updatedAt: string
+}
+
+export interface Category{
+    _id: string
+    _v?:number
+    name: string
     createdAt: string
     updatedAt: string
 }
