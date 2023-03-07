0c064ed1d8ac5c84d61c9bb7aac7e6d6ce75bb30
kulklex <kulklex@gmail.com>
2023-03-07 11:19:38 +0100

    Renamed some components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,39 @@
-import { useContext } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "../App.css";
-import { Context } from "../context/Context";
+import { Category } from "../models/postModel";
 
-export default function Navbar() {
-  const {user, dispatch} = useContext(Context)
-  const admin = user?.user?.isAdmin
+export default function Sidebar() {
+  const [cats, setCats] = useState<Category[]>([])
 
-  const handleLogout = () => {
-    dispatch({type: "LOGOUT"})
-  }
+  useEffect(() => {
+    const getCats = () =>  {
+      axios.get("/categories")
+      .then((res) => {
+        setCats(res.data)
+      })
+      .catch((err) => console.log(err))
+    }
+    getCats()
+  })
   return (
-    <>
-      <div className="w-full h-12 sticky top-0 flex items-center font-serif">
-        <div className="topLeft">
-          <div className="flex flex-row m-2 ">
+    <div className="sideBar bg-white rounded-lg flex flex-col items-center">
+      <div className="sideBarItem flex flex-col items-center">
+        <Link to='/new-category'>
+          <span className="sideBarTitle border-t-2  m-2 p-1 w-[80%]">
+            CATEGORIES
+          </span>
+        </Link>
+        <ul className="sideBarList list-none mb-4">
+          {cats.map((category) => (
+            <Link to={`/?cat=${category.name}`}><li key={category._id} className="sideBarListItem inline-block w-1/2 mt-4 cursor-pointer">{category.name}</li></Link>
+          ))}
+        </ul>
+      </div>
+      <div className="sideBarItem flex flex-col items-center">
+        <span className="sideBarTitle border-t-2  m-2 p-1 w-[80%]">FOLLOW US</span>
+        <div className="sideBarSocial mt-4 flex justify-center">
+        <div className="flex flex-row justify-between m-2 ">
             <a href="#" className="hover:text-gray-900 dark:hover:text-white">
               <svg
                 className="w-6 h-6 text-blue-600 fill-current"
@@ -73,37 +92,7 @@ export default function Navbar() {
             </a>
           </div>
         </div>
-        <div className="topCenter">
-          <ul className="topList flex justify-between m-0 p-0 list-none">
-            <li className="topListItem text-lg font-light"><Link to="/">HOME</Link></li>
-            <li className="topListItem text-lg font-light"><Link to="/">ABOUT</Link></li>
-            <li className="topListItem text-lg font-light"><Link to="/">CONTACT</Link></li>
-            {admin && (<li className="topListItem text-lg font-light"><Link to="/write">WRITE</Link></li>)}
-           {user && ( <li onClick={handleLogout} className="topListItem text-lg font-light cursor-pointer">LOGOUT</li>)}
-          </ul>
-        </div>
-        <div className="topRight flex flex-row items-center justify-center">
-         {user ? (<img
-            className="topImg rounded-2xl bg-cover w-10 h-10"
-            src={user?.user?.profilePic} alt=""
-          />) : (<Link className="link" to='/login'>LOGIN</Link>)}
-          
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="searchIcon mx-4 text-lg text-gray-600 cursor-pointer h-8 w-8"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </div>
       </div>
-    </>
+    </div>
   );
 }
